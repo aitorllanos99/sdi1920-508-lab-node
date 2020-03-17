a2b36eb3b0640ab85b7cee1f0c3b933ee470af40
aitorllanos99 <UO264476@gmail.com>
2020-03-17 10:11:48 +0100

    “SDI-NodeJS- 8,9,10 Recursos estáticos, Peticiones POST y parámetros, Enrutamiento y comodines

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 //Modulos
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 // Variables
 app.set('port', 8081);
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
-
+require("./public/routes/rusuarios.js")(app); // (app, param1, param2, etc.)
+require("./public/routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+app.get('/promo*', function (req, res) {
+    res.send('Respuesta patrón promo* ');
+});
 //Lanzar el servidor
 app.listen(app.get('port'),function(){
     console.log("Servidor activo");
--- public/routes/rcanciones.js
+++ public/routes/rcanciones.js
@@ -1,11 +1,8 @@
 module.exports = function(app) {
     app.get("/canciones", function(req, res) {
-        let respuesta = "";
-        if(req.query.nombre != null)
-            respuesta += 'Nombre: ' + req.query.nombre + '<br>';
-        if(typeof (req.query.autor) != "undefined" )
-            respuesta +=  'Autor: '+ req.query.autor;
-        res.send(respuesta);
+     res.send("Cancion agregada:"+req.body.nombre + "<br>"
+                + " genero : " + req.body.genero + "<br>"
+                + " precio: " +req.body.precio);
     });
 };
 app.get('/canciones/:id', function(req, res) {
