b31b34e90e14502595a6be78a479c787b21938af
aitorllanos99 <UO264476@gmail.com>
2020-03-21 15:36:19 +0100

    SDI-SESION7-EjercicioComplementario1-2 15%

--- routes/rautores.js
+++ routes/rautores.js
@@ -1,6 +1,11 @@
 module.exports = function(app, swig) {
     app.get('/autores/agregar', function (req, res) {
-        if(req.nombre)
+        if(typeof(req.query.nombre) == "undefined")
+            console.log("<Nombre> no enviado en la peticion");
+        if(typeof(req.query.rol) == "undefined")
+            console.log("<Rol> no enviado en la peticion");
+        if(typeof(req.query.grupo) == "undefined")
+            console.log("<Grupo> no enviado en la peticion");
         let respuesta = swig.renderFile('views/autores-agregar.html', {});
         res.send(respuesta);
     });
