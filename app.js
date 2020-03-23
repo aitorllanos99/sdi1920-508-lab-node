973a8e6d1f9471453226fd4f14c5015a50e54f28
aitorllanos99 <UO264476@gmail.com>
2020-03-23 09:18:39 +0100

    SDI-NodeJS-8.2 - Acceso a datos de Mongo desde Node.js

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 let express = require('express');
 let app = express();
 let swig = require('swig');
-
+let mongo = require('mongodb');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 // Variables
 app.set('port', 8081);
+app.set('db','mongodb+srv://admin:admin@cluster0-ftswg.mongodb.net/test?retryWrites=true&w=majority');
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig,mongo); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 app.get('/promo*', function (req, res) {
     res.send('Respuesta patrón promo* ');
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,29 +1,61 @@
-module.exports = function (app, swig) {
-        app.get("/canciones", function (req, res) {
-            let canciones = [{
-                "nombre": "Blue Slide Park",
-                "precio": "1.2"
-            },{
-                "nombre": "Astroworld",
-                "precio": "1.3"
-            }, {
-                "nombre": "Beepongs and bentleys",
-                "precio": "1.1"
-            }];
+module.exports = function (app, swig, mongo) {
+    app.get("/canciones", function (req, res) {
+        let canciones = [{
+            "nombre": "Blue Slide Park",
+            "precio": "1.2"
+        }, {
+            "nombre": "Astroworld",
+            "precio": "1.3"
+        }, {
+            "nombre": "Beepongs and bentleys",
+            "precio": "1.1"
+        }];
 
-            let respuesta = swig.renderFile('views/btienda.html', {
-                vendedor: 'Tienda de canciones',
-                canciones:  canciones
-            });
-            res.send(respuesta);
+        let respuesta = swig.renderFile('views/btienda.html', {
+            vendedor: 'Tienda de canciones',
+            canciones: canciones
         });
+        res.send(respuesta);
+    });
+    app.post("/cancion", function (req, res) {
+        let cancion = {
+            nombre: req.body.nombre,
+            genero: req.body.genero,
+            precio: req.body.precio
+        }
+    });
+
 
 };
 
-module.exports = function(app, swig) {
+module.exports = function (app, swig) {
     app.get('/canciones/agregar', function (req, res) {
         let respuesta = swig.renderFile('views/bagregar.html', {});
         res.send(respuesta);
+    });
+
+    app.post("/cancion", function (req, res) {
+        let cancion = {
+            nombre: req.body.nombre,
+            genero: req.body.genero,
+            precio: req.body.precio
+        }
+// Conectarse
+        mongo.MongoClient.connect(app.get('db'), function (err, db) {
+            if (err) {
+                res.send("Error de conexión: " + err);
+            } else {
+                let collection = db.collection('canciones');
+                collection.insert(cancion, function (err, result) {
+                    if (err) {
+                        res.send("Error al insertar " + err);
+                    } else {
+                        res.send("Agregada id: " + result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
     });
 };
 app.get('/canciones/:id', function (req, res) {
