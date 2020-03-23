aec79609327d1940e4f455f40183d197869babfc
aitorllanos99 <UO264476@gmail.com>
2020-03-23 09:21:06 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 // Variables
 app.set('port', 8081);
 app.set('db','mongodb+srv://admin:admin@cluster0-ftswg.mongodb.net/test?retryWrites=true&w=majority');
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig,mongo); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig,gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig,gestorBD); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 app.get('/promo*', function (req, res) {
     res.send('Respuesta patrón promo* ');
